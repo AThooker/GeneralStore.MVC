eb3eb68a7e4b103f47d9b4200f3e38ebbc157f48
AThooker <austintrent@icloud.com>
2020-08-18 11:15:34 -0400

    Add project files.

---
+++ GeneralStore.MVC/Migrations/202008181515053_Customer.cs
@@ -0,0 +1,30 @@
+namespace GeneralStore.MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Customer : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        CustomerID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false),
+                        LastName = c.String(nullable: false),
+                        FullName = c.String(),
+                    })
+                .PrimaryKey(t => t.CustomerID);
+            
+            AlterColumn("dbo.Products", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Products", "Name", c => c.String());
+            DropTable("dbo.Customers");
+        }
+    }
+}
